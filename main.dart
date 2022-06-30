a5d36ac971f7777526a50f2543f5696464120f34
ns-2002 <look4nandinisingh@gmail.com>
2022-07-01 01:38:59 +0530

    first commit

---
+++ main.dart
@@ -0,0 +1,9 @@
+void main() {
+  int num = 10;
+  String name = "Pankaj";
+  int n = 10;
+  bool isAdmin = true;
+  var abc = 20;
+  print(num is int);
+  print(num is! int);
+}
---
+++ multiples_of_5.dart
@@ -0,0 +1,6 @@
+void main(){
+    List mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    mylist.forEach((i) {
+      print(i * 5);
+    });
+}
